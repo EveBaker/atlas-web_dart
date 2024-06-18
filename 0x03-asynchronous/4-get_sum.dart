63513cdc38245eccac481edea981efab42a71d6a
Eve Baker <103212241+EveBaker@users.noreply.github.com>
2024-06-18 19:19:41 +0000

    task 4 done

--- 0x03-asynchronous/4-get_sum.dart
+++ 0x03-asynchronous/4-get_sum.dart
@@ -1,27 +1,27 @@
 import 'dart:convert';
 import '4-util.dart';
 
-Future<int> calculateTotal() async {
-  try {
-    // get data
-    String userData = await fetchUserData();
-    Map<String, dynamic> userMap = jsonDecode(userData);
-    String userId = userMap['id'];
+Future<double> calculateTotal() async {
+    try {
+        //get user
+        String userData = await fetchUserData();
+        Map<String, dynamic> userMap = jsonDecode(userData);
+        String userId = userMap['id'];
 
-    // get orders
-    String ordersData = await fetchUserOrders(userId);
-    List<dynamic> ordersList = jsonDecode(ordersData);
 
-    // calculate price
-    double totalPrice = 0.0;
-    for (var product in ordersList) {
-      String priceData = await fetchProductPrice(product);
-      double price = double.parse(priceData);
-      totalPrice += price;
+        //get orders
+         String ordersData = await fetchUserOrders(userId);
+        List<dynamic> ordersList = jsonDecode(ordersData);
+
+        //calculate price
+        double totalPrice = 0.0;
+            for (var product in ordersList) {
+            String priceData = await fetchProductPrice(product);
+            double price = double.parse(priceData);
+            totalPrice += price;
+        }
+        return totalPrice.toInt(); 
+        } catch (error) {
+            return -1;
     }
-
-    return totalPrice.toInt();
-  } catch (error) {
-    return -1;
-  }
 }
